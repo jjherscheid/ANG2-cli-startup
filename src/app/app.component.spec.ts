e202746438fe03d72df173cbef3128edecddaf12
Johan Herscheid <johan.herscheid@priva.nl>
2017-02-08 11:00:08 +0100

    Fix unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,16 +23,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'My App Component'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('My App Component');
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in a a.navbar-brand tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('a.navbar-brand').textContent).toContain('My App Component');
   }));
 });
